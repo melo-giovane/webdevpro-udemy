c2756596042e918e11dff8e3a36065f6cdbe758e
Giovane <giovanec.melo@hotmail.com>
2025-04-14 00:10:49 -0300

    server.js

--- nodejs/server/server.js
+++ nodejs/server/server.js
@@ -6,29 +6,36 @@ const path = require("path");
 const port = 8080;
 
 const server = http.createServer((req, res) => {
-  const filePath = path.join(__dirname, req.url === '/' ? "index.html" : "request.url")
+  const filePath = path.join(
+    __dirname,
+    req.url === "/" ? "index.html" : req.url
+  );
+  
 
-  const extName = String(path.extname(filePath)).toLowerCase()
+  const extName = String(path.extname(filePath)).toLowerCase();
 
   const mimeTypes = {
-    '.html': 'text/html',
-    '.css': 'text/css',
-    '.js': 'text/javascript',
-    '.png': 'text/png',
-  }
+    ".html": "text/html",
+    ".css": "text/css",
+    ".js": "text/javascript",
+    ".png": "text/png",
+  };
 
-  const contentType = mimeTypes[extName] || 'application/octet-stream';
+  const contentType = mimeTypes[extName] || "application/octet-stream";
 
   fs.readFile(filePath, (err, content) => {
-    if(err) {
-      
-    } else{
-      res.writeHead()
-
+    if (err) {
+      if (err.code === "ENOENT") {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        res.end("404: File Not Found BRooooo");
+      }
+    } else {
+      res.writeHead(200, { "Content-Type": contentType });
+      res.end(content, "utf-8");
     }
-  })
+  });
 });
 
 server.listen(port, () => {
-  console.log(`Ser ver is listening on port ${port}`);
+  console.log(`Server is listening on port ${port}`);
 });
